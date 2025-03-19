77b0e17c32eaad587bbe36dde319878d2e574513
Essien Emmanuel <essienemma79@gmail.com>
2025-03-19 12:12:37 -0700

    style added

--- script.js
+++ script.js
@@ -24,46 +24,49 @@ let score = JSON.parse(localStorage.getItem("score")) || {
 // }
 
 function onRock() {
-  playGame(rock);
+  playGame("rock");
 }
 function onPaper() {
-  playGame(paper);
+  playGame("paper");
 }
 function onScissors() {
-  playGame(scissors);
+  playGame("scissors");
 }
 
 function playGame(playerMove) {
   const computerMove = compMove();
+  console.log(playerMove);
 
   let result;
+  let gameMove;
 
-  if (playerMove === rock) {
-    if (computerMove === rock) {
+  if (playerMove === "rock") {
+    if (computerMove === "rock") {
       result = "Its a draw";
-    } else if (computerMove === paper) {
+      // gameMove = "rock";
+    } else if (computerMove === "paper") {
       result = "HAHAHA you lost";
-    } else {
+    } else if (computerMove === "scissors") {
       result = "Damn you won";
     }
   }
 
-  if (playerMove === paper) {
-    if (computerMove === rock) {
+  if (playerMove === "paper") {
+    if (computerMove === "rock") {
       result = "Damn you won";
-    } else if (computerMove === paper) {
+    } else if (computerMove === "paper") {
       result = "Its a draw";
-    } else {
+    } else if (computerMove === "scissors") {
       result = "HAHAHA you lost";
     }
   }
 
-  if (playerMove === scissors) {
-    if (computerMove === rock) {
+  if (playerMove === "scissors") {
+    if (computerMove === "rock") {
       result = "HAHAHA you lost";
-    } else if (computerMove === paper) {
+    } else if (computerMove === "paper") {
       result = "Damn you won";
-    } else {
+    } else if (computerMove === "scissors") {
       result = "Its a draw";
     }
   }
@@ -78,20 +81,31 @@ function playGame(playerMove) {
 
   localStorage.setItem("score", JSON.stringify(score));
 
-  alert(`${result}
-  Wins: ${score.wins}, Losses:${score.losses}, Ties: ${score.draw}`);
+  // alert(`${result}
+  // Wins: ${score.wins}, Losses:${score.losses}, Ties: ${score.draw}
+  // `);
+
+  scoreUpdate();
+
+  document.querySelector(".game-result").innerHTML = `${result}`;
+
+  document.querySelector(
+    ".game-move"
+  ).innerHTML = ` You <img class="move-icon" src="images/${playerMove}-emoji.png" alt="" />
+   Computer <img class="move-icon" src="images/${computerMove}-emoji.png" alt="" />
+  `;
 }
 
 function compMove() {
-  const randomNumber = Math.ceil(Math.random() * 3);
+  const randomNumber = Math.ceil(Math.random() * 9);
   let computerMove;
 
-  if (randomNumber === 1) {
-    computerMove = rock;
-  } else if (randomNumber === 2) {
-    computerMove = paper;
+  if (randomNumber <= 3) {
+    computerMove = "rock";
+  } else if (randomNumber > 3 && randomNumber <= 6) {
+    computerMove = "paper";
   } else {
-    computerMove = scissors;
+    computerMove = "scissors";
   }
 
   return computerMove;
@@ -99,10 +113,20 @@ function compMove() {
 
 function onClear() {
   (score.wins = 0), (score.losses = 0), (score.draw = 0);
-  alert(`Game Reset
-    Wins: ${score.wins}, Losses:${score.losses}, Ties: ${score.draw}`);
+
+  scoreUpdate();
+
+  document.querySelector(".game-result").innerHTML = " Game reset";
 
   localStorage.removeItem("score");
+}
+
+function scoreUpdate() {
+  const gameInfo = document.querySelector(".game-info");
+
+  const gameScore = document.querySelector(".game-score");
+  gameScore.innerHTML = `
+   Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.draw}`;
 }
 
 rock.addEventListener("click", onRock);
